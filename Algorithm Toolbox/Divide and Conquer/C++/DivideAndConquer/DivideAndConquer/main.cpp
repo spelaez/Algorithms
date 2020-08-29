93f8dd6a2d0763255c575dc425adeb3cd449ff14
Santiago <santirua@gmail.com>
2020-08-29 16:30:18 -0500

    initial project for divide and conquer

---
+++ Algorithm Toolbox/Divide and Conquer/C++/DivideAndConquer/DivideAndConquer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DivideAndConquer
+//
+//  Created by Santiago Pelaez Rua on 29/08/20.
+//  Copyright © 2020 Santiago Pelaez Rua. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
