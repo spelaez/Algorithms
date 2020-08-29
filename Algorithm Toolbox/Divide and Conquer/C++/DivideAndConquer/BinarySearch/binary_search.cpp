0233093caaf012c6e950a9d06d64d86e36019e46
Santiago <santirua@gmail.com>
2020-08-29 16:48:41 -0500

    binary search

--- Algorithm Toolbox/Divide and Conquer/C++/DivideAndConquer/BinarySearch/binary_search.cpp
+++ Algorithm Toolbox/Divide and Conquer/C++/DivideAndConquer/BinarySearch/binary_search.cpp
@@ -8,8 +8,45 @@
 
 #include <iostream>
 
+int binarySearch(int a[], int k, int l, int r) {
+    while (l <= r) {
+        int middle = (l + ((r-l)/2));
+        
+        if (a[middle] == k) {
+            return middle;
+        } else if (k > a[middle]) {
+            l = middle + 1;
+        } else {
+            r = middle - 1;
+        }
+    }
+    
+    return -1;
+}
+
+int binarySearch(int a[], int n, int k) {
+    return binarySearch(a, k, 0, n - 1);
+}
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n;
+    std::cin >> n;
+    
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    
+    int k;
+    std::cin >> k;
+    for (int i = 0; i < k; i++) {
+        int ki;
+        std::cin >> ki;
+        
+        std::cout << binarySearch(a, n, ki) << " ";
+    }
+    
+    std::cout << std::endl;
     return 0;
 }
