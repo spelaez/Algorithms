554cbe09f783bc37d70273c20ce27dded241757c
Santiago <santirua@gmail.com>
2020-09-18 23:17:52 -0500

    Data structures 1st week project creation

---
+++ Data Structures/Basic Data Structures/C++/BasicDataStructures/BasicDataStructures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BasicDataStructures
+//
+//  Created by Santiago Pelaez Rua on 18/09/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
